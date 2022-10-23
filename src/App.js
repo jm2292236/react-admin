0bca8787087507c1d106019fe11c1ec1711ff850
Jaime Montes de Oca <jaime0422@live.com>
2022-10-23 00:28:25 -0700

    Initial commit - Project structure - theme settings

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { multiplyDuration } from '@fullcalendar/core';
+import { ColorModeContext, useMode } from './theme';
+
+import { CssBaseline, ThemeProvider } from '@mui/material'
+// CssBaseline is going to reset our CSS to its defaults that we'll probably need
+// ThemeProvider is going to provide the ability to pass in the themes into material UI
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [theme, colorMode] = useMode();
+
+    return (
+        <ColorModeContext.Provider value={colorMode}>
+            <ThemeProvider theme={theme}>
+                <CssBaseline />     {/* Reset CSS to its defaults */}
+                <div className="app">
+                    This is the React Admin Dashboard
+                </div>
+            </ThemeProvider>
+        </ColorModeContext.Provider>
+    );
 }
 
 export default App;
